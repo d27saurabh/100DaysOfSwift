8cf9e30e70fc0608e9e8fc6aac645697bd000ae6
Saurabh Deshpande <d27saurabh@gmail.com>
2020-08-14 15:15:13 -0400

    Day 25: Project 4: part two

--- Project4/Project4/ViewController.swift
+++ Project4/Project4/ViewController.swift
@@ -13,6 +13,8 @@ class ViewController: UIViewController, WKNavigationDelegate {
     
     //properties
     var webView: WKWebView!
+    var progressView: UIProgressView!
+    var websites = ["apple.com", "hackingwithswift.com"]
     
     override func loadView() {
         webView = WKWebView()
@@ -23,20 +25,38 @@ class ViewController: UIViewController, WKNavigationDelegate {
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        
+        //add Open button to the top right in the naviagtion bar
         navigationItem.rightBarButtonItem = UIBarButtonItem(title: "Open", style: .plain, target: self, action: #selector(openTapped))
         
+        //create two uibarbutton items and add to toolbaritems and show toolbaritems
+        let spacer = UIBarButtonItem(barButtonSystemItem: .flexibleSpace, target: nil, action: nil)
+        let refresh = UIBarButtonItem(barButtonSystemItem: .refresh, target: webView, action: #selector(webView.reload))
         
-        let url = URL(string: "https://www.hackingwithswift.com")!
+        //create a progress view
+        progressView = UIProgressView(progressViewStyle: .default)
+        progressView.sizeToFit()
+        let progressButton = UIBarButtonItem(customView: progressView)
+        
+        toolbarItems = [progressButton, spacer, refresh]
+        navigationController?.isToolbarHidden = false
+        
+        //show the jackingwithswift website when the app is loaded
+        let url = URL(string: "https://" + websites[0])!
         webView.load(URLRequest(url: url))
+        
+        //set the forward and backward navigation gesture
         webView.allowsBackForwardNavigationGestures = true
+        
+        //add an observer to for new value of the estimatedProgress
+        webView.addObserver(self, forKeyPath: #keyPath(WKWebView.estimatedProgress), options: .new, context: nil)
     }
     
     @objc func openTapped() {
-        let apple = "apple.com"
-        let omegaLeague = "liquipedia.net/dota2/OMEGA_League/1/Europe/Immortal_Division"
         let ac = UIAlertController(title: "Open page...", message: nil, preferredStyle: .actionSheet)
-        ac.addAction(UIAlertAction(title: apple, style: .default, handler: openPage))
-        ac.addAction(UIAlertAction(title: omegaLeague, style: .default, handler: openPage))
+        for website in websites {
+            ac.addAction(UIAlertAction(title: website, style: .default, handler: openPage))
+        }
         ac.addAction(UIAlertAction(title: "Cancel", style: .cancel))
         ac.popoverPresentationController?.barButtonItem = navigationItem.rightBarButtonItem
         present(ac, animated: true)
@@ -50,6 +70,28 @@ class ViewController: UIViewController, WKNavigationDelegate {
     
     func webView(_ webView: WKWebView, didFinish navigation: WKNavigation!) {
         title = webView.title
+    }
+    
+    
+    
+    func webView(_ webView: WKWebView, decidePolicyFor navigationAction: WKNavigationAction, decisionHandler: @escaping (WKNavigationActionPolicy) -> Void) {
+        let url = navigationAction.request.url
+        
+        if let host = url?.host {
+            for website in websites {
+                if host.contains(website) {
+                    decisionHandler(.allow)
+                    return
+                }
+            }
+        }
+        decisionHandler(.cancel)
+    }
+    
+    override func observeValue(forKeyPath keyPath: String?, of object: Any?, change: [NSKeyValueChangeKey : Any]?, context: UnsafeMutableRawPointer?) {
+        if keyPath == "estimatedProgress" {
+            progressView.progress = Float(webView.estimatedProgress)
+        }
     }
 
 

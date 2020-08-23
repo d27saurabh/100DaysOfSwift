3635f63267c912e4de0fcba1aa61f464bee9ff76
d27saurabh <d27saurabh@gmail.com>
2020-08-22 23:20:40 -0400

    Day 33: Project 7 - part one

---
+++ Project7/Project7/Petitions.swift
@@ -0,0 +1,13 @@
+//
+//  Petitions.swift
+//  Project7
+//
+//  Created by Saurabh Deshpande on 8/22/20.
+//  Copyright © 2020 Saurabh Deshpande. All rights reserved.
+//
+
+import Foundation
+
+struct Petitions: Codable {
+    var results: [Petition]
+}

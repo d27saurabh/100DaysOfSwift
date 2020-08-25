37f514d3449140395e2e62f53d018f6c54964cc2
d27saurabh <d27saurabh@gmail.com>
2020-08-24 23:11:36 -0400

    Day 35: Project 7 - part three

--- Project7/Project7/ViewController.swift
+++ Project7/Project7/ViewController.swift
@@ -12,11 +12,23 @@ class ViewController: UITableViewController {
     
     // MARK: - Properties
     var petitions = [Petition]()
+    var filteredPetitions = [Petition]()
+    var filterBarButton : UIBarButtonItem?
+    var clearBarButton: UIBarButtonItem?
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
         let urlString: String
+        
+        let credits = UIBarButtonItem(title: "Credits", style: .plain, target: self, action: #selector(showCredits))
+        navigationItem.leftBarButtonItem = credits
+        
+        filterBarButton = UIBarButtonItem(barButtonSystemItem: .edit, target: self, action: #selector(filterOption))
+        if let filterBarButton = filterBarButton {
+            navigationItem.rightBarButtonItems = [filterBarButton]
+        }
+        
         
         if navigationController?.tabBarItem.tag == 0 {
             urlString = "https://www.hackingwithswift.com/samples/petitions-1.json"
@@ -45,17 +57,18 @@ class ViewController: UITableViewController {
         
         if let jsonPetitions = try? decoder.decode(Petitions.self, from: json) {
             petitions = jsonPetitions.results
+            filteredPetitions = petitions
             tableView.reloadData()
         }
     }
     
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return petitions.count
+        return filteredPetitions.count
     }
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
-        let petition = petitions[indexPath.row]
+        let petition = filteredPetitions[indexPath.row]
         cell.textLabel?.text = petition.title
         cell.detailTextLabel?.text = petition.body
         return cell
@@ -65,6 +78,51 @@ class ViewController: UITableViewController {
         let vc = DetailViewController()
         vc.detailItem = petitions[indexPath.row]
         navigationController?.pushViewController(vc, animated: true)
+    }
+    
+    @objc func showCredits() {
+        let ac = UIAlertController(title: "Data is from", message: "We The People API of the Whitehouse", preferredStyle: .alert)
+        ac.addAction(UIAlertAction(title: "OK", style: .default))
+        present(ac, animated: true)
+    }
+    
+    @objc func filterOption() {
+        let ac = UIAlertController(title: "Filter topic", message: nil, preferredStyle: .alert)
+        ac.addTextField()
+        let filterAction = UIAlertAction(title: "Filter", style: .default) {
+            [weak self, weak ac] _ in
+            guard let userString = ac?.textFields?[0].text else { return }
+            self?.filterOn(userString)
+        }
+        ac.addAction(filterAction)
+        present(ac, animated: true)
+    }
+    
+    func filterOn(_ userString: String) {
+        filteredPetitions.removeAll(keepingCapacity: true)
+        for petition in petitions {
+            if petition.title.lowercased().contains(userString.lowercased()) || petition.body.lowercased().contains(userString.lowercased()) {
+                filteredPetitions.append(petition)
+            }
+        }
+        clearBarButton = UIBarButtonItem(title: "Clear", style: .done, target: self, action: #selector(clearFilter))
+        if let clearBarButton = clearBarButton {
+            navigationItem.rightBarButtonItems = [clearBarButton]
+            
+        }
+        
+        
+        tableView.reloadData()
+    }
+    
+    @objc func clearFilter() {
+        filteredPetitions.removeAll(keepingCapacity: true)
+        filteredPetitions = petitions
+        if let filterBarButton = filterBarButton {
+            navigationItem.rightBarButtonItems = [filterBarButton]
+            
+        }
+        tableView.reloadData()
     }
     
 }

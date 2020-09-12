d5644df109c4f09ee4e36ab573618265d3a75c25
Saurabh Deshpande <d27saurabh@gmail.com>
2020-09-12 00:52:56 -0400

    Day 42: Project 10 - part one

---
+++ Project10/Project10/ViewController.swift
@@ -0,0 +1,29 @@
+//
+//  ViewController.swift
+//  Project10
+//
+//  Created by Saurabh Deshpande on 9/12/20.
+//  Copyright © 2020 Saurabh Deshpande. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UICollectionViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+    override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 10
+    }
+    
+    override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "Person", for: indexPath) as? PersonCell else {
+            fatalError("Unable to dequeue PersonCell.")
+        }
+        return cell
+    }
+}
+

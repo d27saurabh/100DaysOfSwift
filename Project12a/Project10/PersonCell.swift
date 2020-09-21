24a587914abf17181ac71062bd84427f3deff174
Saurabh Deshpande <d27saurabh@gmail.com>
2020-09-20 22:13:17 -0400

    Day 48: Project 12 - part one

---
+++ Project12a/Project10/PersonCell.swift
@@ -0,0 +1,15 @@
+//
+//  PersonCell.swift
+//  Project10
+//
+//  Created by Saurabh Deshpande on 9/12/20.
+//  Copyright © 2020 Saurabh Deshpande. All rights reserved.
+//
+
+import UIKit
+
+class PersonCell: UICollectionViewCell {
+  
+    @IBOutlet var imageView: UIImageView!
+    @IBOutlet var name: UILabel!
+}
